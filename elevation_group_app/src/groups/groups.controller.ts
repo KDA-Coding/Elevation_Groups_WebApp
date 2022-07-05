4ac3678b2c4826a3c92c144a53a0faff52519732
KDA-Coding <59184838+KDA-Coding@users.noreply.github.com>
2022-07-04 20:35:50 -0400

    Modifications to Service data and Controller methods

--- elevation_group_app/src/groups/groups.controller.ts
+++ elevation_group_app/src/groups/groups.controller.ts
@@ -1,17 +1,26 @@
-import { Get } from '@nestjs/common';
+import { Get, NotFoundException, Param } from '@nestjs/common';
 import { Controller, NotImplementedException } from '@nestjs/common';
 import { GroupsService } from './groups.service';
 
 @Controller('groups')
 export class GroupsController {
 
-    constructor(private readonly groupService: GroupsService) {
-
-    }
+    constructor(private readonly groupService: GroupsService) { }
 
     @Get()
     getGroups() {
         return this.groupService.findAllGroups();
+    }
+
+    @Get(':groupId')
+    getGroupById(@Param('groupId') id:number) {
+        let group = this.groupService.findGroupById(id);
+
+        if (!group) {
+            throw new NotFoundException(`Task with Id ${id} not found`);
+        }
+
+        return group;
     }
 
 }
--- elevation_group_app/src/groups/groups.service.ts
+++ elevation_group_app/src/groups/groups.service.ts
@@ -5,13 +5,13 @@ import { Injectable } from '@nestjs/common';
 export class GroupsService {
     _groups: GroupModel[] = [
         { id: 1,
-        campus: "Asheville",
-        demographic: "Men",
-        group_type: "Marriage",
-        meeting_date: "2020-11-02T19:16:57.985Z",
-        zip_code: 28105,
-        description: "A Men's group to help guide " + 
-        "married men through marriage hardships."},
+            campus: "Asheville",
+            demographic: "Men",
+            group_type: "Marriage",
+            meeting_date: "2020-11-02T19:16:57.985Z",
+            zip_code: 28105,
+            description: "A Men's group to help guide " + 
+            "married men through marriage hardships."},
         { id: 2,
             campus: "Ballantyne",
             demographic: "Singles",
@@ -21,12 +21,12 @@ export class GroupsService {
             description: "A singles group for growing " + 
             "in the word and building community."},
         { id: 3,
-        campus: "Blakeny",
-        demographic: "Women",
-        group_type: "Book Study",
-        meeting_date: "2020-09-02T19:16:57.985Z",
-        zip_code: 28276,
-        description: "Women studying books from Christian authors."},
+            campus: "Blakeny",
+            demographic: "Women",
+            group_type: "Book Study",
+            meeting_date: "2020-09-02T19:16:57.985Z",
+            zip_code: 28276,
+            description: "Women studying books from Christian authors."},
         { id: 4,
             campus: "Columbia",
             demographic: "Women",
@@ -36,12 +36,12 @@ export class GroupsService {
             description: "A Women's group for studying " + 
             "Christian novels and growing in faith."},
         { id: 5,
-        campus: "Concord",
-        demographic: "Everyone Welcome",
-        group_type: "Parenting",
-        meeting_date: "2020-11-02T19:16:57.985Z",
-        zip_code: 28276,
-        description: "A group for all parents; new and experienced."},
+            campus: "Concord",
+            demographic: "Everyone Welcome",
+            group_type: "Parenting",
+            meeting_date: "2020-11-02T19:16:57.985Z",
+            zip_code: 28276,
+            description: "A group for all parents; new and experienced."},
         { id: 6,
             campus: "Elevation Online",
             demographic: "Everyone Welcome",
@@ -50,17 +50,21 @@ export class GroupsService {
             zip_code: 28105,
             description: "A group for discussing the weekly sermons"},
         { id: 7,
-        campus: "Gaston",
-        demographic: "Singles",
-        group_type: "Christian Basics",
-        meeting_date: "2020-10-02T19:16:57.985Z",
-        zip_code: 28275,
-        description: "A group for new and continuing Christian" +
-        "singles to gain fellowship and grow in faith."}
+            campus: "Gaston",
+            demographic: "Singles",
+            group_type: "Christian Basics",
+            meeting_date: "2020-10-02T19:16:57.985Z",
+            zip_code: 28275,
+            description: "A group for new and continuing Christian" +
+            "singles to gain fellowship and grow in faith."}
     ]
 
     findAllGroups(): GroupModel[] {
         return this._groups;
+    }
+
+    findGroupById(id:number) {
+        return this._groups.find(g => g.id == id);
     }
 
 }
